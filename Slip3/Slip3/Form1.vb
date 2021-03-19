446e49102ed97f38242bf025f354019a767210f4
Anu Keswani <63454977+anukeswani1406@users.noreply.github.com>
2021-03-19 10:42:51 +0530

    Add files via upload

---
+++ Slip3/Slip3/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If Label1.Left > 400 Then
+            Label1.Left = 10
+        Else
+            Label1.Left = Label1.Left + 15
+        End If
+    End Sub
+End Class
---
+++ Slip4/Slip4/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim n As Integer
+        Dim i As Integer
+        n = CInt(InputBox("Enter Number"))
+
+        For i = 1 To 10
+            ListBox1.Items.Add(n * i)
+        Next
+    End Sub
+End Class
