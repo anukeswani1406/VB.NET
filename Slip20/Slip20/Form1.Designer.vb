aa5569aa499e066f2e649673342dc1f3c88fad04
Anu Keswani <63454977+anukeswani1406@users.noreply.github.com>
2021-03-19 10:50:07 +0530

    Add files via upload

---
+++ Slip20/Slip20/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FontStyleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FontStyleToolStripMenuItem, Me.FontColorToolStripMenuItem, Me.SaveToolToolStripMenuItem, Me.OpenToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(776, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FontStyleToolStripMenuItem
+        '
+        Me.FontStyleToolStripMenuItem.Name = "FontStyleToolStripMenuItem"
+        Me.FontStyleToolStripMenuItem.Size = New System.Drawing.Size(68, 20)
+        Me.FontStyleToolStripMenuItem.Text = "FontStyle"
+        '
+        'FontColorToolStripMenuItem
+        '
+        Me.FontColorToolStripMenuItem.Name = "FontColorToolStripMenuItem"
+        Me.FontColorToolStripMenuItem.Size = New System.Drawing.Size(72, 20)
+        Me.FontColorToolStripMenuItem.Text = "FontColor"
+        '
+        'SaveToolToolStripMenuItem
+        '
+        Me.SaveToolToolStripMenuItem.Name = "SaveToolToolStripMenuItem"
+        Me.SaveToolToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
+        Me.SaveToolToolStripMenuItem.Text = "Save"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(59, 55)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(343, 119)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(776, 339)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FontStyleToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FontColorToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveToolToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ Slip20/Slip20/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim f As New FontDialog
+    Dim c As New ColorDialog
+    Dim s As New SaveFileDialog
+    Dim op As New OpenFileDialog
+    Dim filename As String
+    Dim searh As String
+    Dim sw As StreamWriter
+    Dim sr As StreamReader
+
+    'Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+    '    RichTextBox1.Size = Me.Size
+    'End Sub
+
+
+    Private Sub FontStyleToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FontStyleToolStripMenuItem.Click
+        If f.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            RichTextBox1.Font = f.Font
+        End If
+    End Sub
+
+    Private Sub FontColorToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FontColorToolStripMenuItem.Click
+        If c.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            RichTextBox1.ForeColor = c.Color
+        End If
+    End Sub
+
+    Private Sub SaveToolToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveToolToolStripMenuItem.Click
+        If s.ShowDialog = Windows.Forms.DialogResult.OK Then
+            filename = s.FileName
+            sw = New StreamWriter(filename)
+            sw.Write(RichTextBox1.Text)
+            sw.Close()
+        End If
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OpenToolStripMenuItem.Click
+        If op.ShowDialog = Windows.Forms.DialogResult.OK Then
+            'Process.Start(op.FileName)
+            sr = New StreamReader(op.OpenFile)
+            RichTextBox1.Text = sr.ReadToEnd()
+            sr.Close()
+        End If
+    End Sub
+End Class
